50950a1a624a15b9a296614291ba1bcfa685bd7c
Irem Taşgın <tasginoguz@gmail.com>
2022-10-21 16:55:09 +0300

    ikicisoru

---
+++ src/Lesson002/Question2.java
@@ -0,0 +1,8 @@
+package Lesson002;
+
+public class Question2 {
+	public static void main(String[] args) {
+		System.out.println("Hello Gihub");
+	}
+
+}
---
+++ src/lesson001/Question1.java
@@ -0,0 +1,8 @@
+package lesson001;
+
+public class Question1 {
+	public static void main(String[] args) {
+		System.out.println("Hello Gihub");
+	}
+
+}
