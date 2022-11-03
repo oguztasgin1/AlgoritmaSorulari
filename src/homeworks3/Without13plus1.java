cc91ffd35def805d9de6362207d8fa5cabe2d984
oguz11 <47160639+oguztasgin1@users.noreply.github.com>
2022-11-03 17:20:03 +0300

    Update Without13plus1.java

--- src/homeworks3/Without13plus1.java
+++ src/homeworks3/Without13plus1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package homeworks3;
 
 public class Without13plus1 {
@@ -31,37 +30,3 @@ public class Without13plus1 {
 		System.out.println(toplam);
 	}
 }
-=======
-package homeworks3;
-
-public class Without13plus1 {
-
-	public static void main(String[] args) {
-		// Bir dizide bulunan 13 ve 13 ten sonra gelen sayıları eklemeden
-		// toplam sonucu yazan program.
-		
-		int [] numbers = {1, 13, 13, 13, 2, 5, 13, 1};
-		
-		int toplam = 0;
-		boolean flag = false;
-		boolean durum = true;
-		
-		for (int i = 0; i < numbers.length; i++) {
-			if(numbers[i]!=13) {
-				flag = true;
-				if(flag && durum) {
-					toplam += numbers[i];
-				}
-				else {
-					durum = true;
-				}
-			}
-			else {
-				durum = false;
-			}
-		}
-
-		System.out.println(toplam);
-	}
-}
->>>>>>> 67270bbce248cca51cef27d028083b2d1aec1a14
